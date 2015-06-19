64cef9cfeace10af951368b90351431f6b315701
Derek Cacciotti <derekcacciotti@Dereks-iMac.local>
2015-06-18 22:47:51 -0400

    testing

---
+++ gittest/gittest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Derek Cacciotti on 6/18/15.
+//  Copyright (c) 2015 Derek Cacciotti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("test")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
